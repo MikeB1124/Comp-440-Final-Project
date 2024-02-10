ab22c21881dda1875cbe12e28902398119481fe9
MikeB1124 <balianmichael@gmail.com>
2024-02-10 09:42:54 -0800

    adding more info on the db creation and deletion alerts

--- sql/create_db.php
+++ sql/create_db.php
@@ -4,7 +4,7 @@ require_once "config.php";
 // Create database
 $sql = "CREATE DATABASE test";
 if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
+    echo "Database created successfully\nVisit http://localhost:80/phpmyadmin page to check result";
 } else {
     echo "Error creating database: " . $conn->error;
 }
--- sql/delete_db.php
+++ sql/delete_db.php
@@ -4,7 +4,7 @@ require_once "config.php";
 // Create database
 $sql = "DROP DATABASE test";
 if ($conn->query($sql) === TRUE) {
-    echo "Database deleted successfully";
+    echo "Database deleted successfully\nVisit http://localhost:80/phpmyadmin page to check result";
 } else {
     echo "Error deleting database: " . $conn->error;
 }
