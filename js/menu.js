8c0de0569263a0934de1843b022f74da87bc7af8
MikeB1124 <balianmichael@gmail.com>
2024-04-16 21:50:15 -0700

    menu now displaying on the home page

--- js/menu.js
+++ js/menu.js
@@ -36,7 +36,6 @@ selectLocation.addEventListener('click', function(){
     cleanUpPreviousLocation()
     if(selectLocation.value != "0"){
         createMenuHeader(selectLocation.value)
-        // getSections(selectLocation.value)
     }else{
         let menuContainer = document.querySelector(".menu-container")
         let defaultHeader = document.createElement("h1")
@@ -45,22 +44,6 @@ selectLocation.addEventListener('click', function(){
     }
 })
 
-
-//Get Sections
-function getSections(locationId){
-    $.ajax({
-        type: "GET",
-        url: `controllers/section/get_sections.php?locationId=${locationId}`,
-        success: function(response){
-            let sections = JSON.parse(response);
-            if (sections.length > 0){
-                console.log(sections)
-                createBaseTable(sections)
-                // getItems(sections)
-            }
-        }
-    });
-}
 
 function createMenuHeader(locationId){
     $.ajax({
@@ -77,20 +60,105 @@ function createMenuHeader(locationId){
                     locationAddress.innerText = `${location["Address"]}, ${location["City"]} ${location["State"]} ${location["Postal_Code"]}`
                     let locationPhoneNumber = document.createElement("h2")
                     locationPhoneNumber.innerText = location["Phone"]
+                    locationPhoneNumber.style.marginBottom = "64px";
 
                     menuContainer.appendChild(locationName)
                     menuContainer.appendChild(locationAddress)
                     menuContainer.appendChild(locationPhoneNumber)
                 }
             });
+            getSections(locationId)
         }
     });
 }
 
+
+//Get Sections
+function getSections(locationId){
+    $.ajax({
+        type: "GET",
+        url: `controllers/section/get_sections.php?locationId=${locationId}`,
+        success: function(response){
+            let sections = JSON.parse(response);
+            if (sections.length > 0){
+                createBaseTable(sections)
+                getItems(sections)
+            }
+        }
+    });
+}
+
+
 //Create Base Table for Menu
 function createBaseTable(sections){
+    let menuContainer = document.querySelector(".menu-container")
     sections.forEach(section => {
-        let menuContainer = document.querySelector(".menu-container")
-        console.log("hello world")
+        let columnContainer = document.createElement("div")
+        columnContainer.setAttribute("class", "col")
+        columnContainer.style.marginBottom = "64px"
+
+        let sectionHeader = document.createElement("h1")
+        sectionHeader.innerText = section["Section_Name"]
+
+        let sectionContainer = document.createElement("div")
+        sectionContainer.setAttribute("class", `section-container section-${section["ID"]}`)
+        sectionContainer.style.display = "flex"
+        sectionContainer.style.justifyContent = "center"
+        sectionContainer.style.gap = "32px"
+        sectionContainer.style.flexWrap = "wrap"
+
+        columnContainer.appendChild(sectionHeader)
+        columnContainer.appendChild(sectionContainer)
+        menuContainer.appendChild(columnContainer)
+    });
+}
+
+
+//Get Items
+function getItems(sections){
+    sections.forEach(section => {
+        $.ajax({
+            type: "GET",
+            url: `controllers/item/get_items.php?sectionId=${section["ID"]}`,
+            success: function(response){
+                let items = JSON.parse(response);
+                insertItems(items)
+            }
+        });
+    });
+}
+
+function insertItems(items){
+    items.forEach(item => {
+        let sectionContainer = document.querySelector(`.section-${item["Section_ID"]}`)
+
+        let cardContainer = document.createElement("div")
+        cardContainer.setAttribute("class", "card")
+        cardContainer.style.width = "250px"
+        cardContainer.style.height = "300px"
+
+        let img = document.createElement("img")
+        img.setAttribute("class", "card-img-top")
+        img.src = item["Image_Url"]
+
+        cardContainer.appendChild(img)
+
+        let cardBody = document.createElement("div")
+        cardBody.setAttribute("class", "card-body")
+
+        let cardHeader = document.createElement("h5")
+        cardHeader.setAttribute("class", "card-title")
+        cardHeader.innerText = `${item["Item_Name"]} ($${item["Price"]})`
+
+        let cardParagraph = document.createElement("p")
+        cardParagraph.setAttribute("class", "card-text")
+        cardParagraph.innerText = item["Description"]
+
+        cardBody.appendChild(cardHeader)
+        cardBody.appendChild(cardParagraph)
+
+        cardContainer.appendChild(cardBody)
+
+        sectionContainer.appendChild(cardContainer)
     });
 }
