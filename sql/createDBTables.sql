89e12c81d1ca1ec50ec83e20151866d4a5069331
MikeB1124 <balianmichael@gmail.com>
2024-04-21 11:56:41 -0700

    adding table columns and checks

--- sql/createDBTables.sql
+++ sql/createDBTables.sql
@@ -3,12 +3,16 @@ USE RestaurantDB;
 
 CREATE TABLE `Location` (
     `ID` INT NOT NULL AUTO_INCREMENT,
-    `Location_Name` VARCHAR(20) NOT NULL,
+    `Location_Name` VARCHAR(30) NOT NULL,
     `Address` VARCHAR(50) NOT NULL,
     `City` VARCHAR(50) NOT NULL,
     `Postal_Code` INT,
-    `State` VARCHAR(2) NOT NULL,
-    `Phone` VARCHAR(10) NOT NULL,
+    `State` VARCHAR(10) NOT NULL
+    	CHECK (LENGTH(`State`) = 2),
+    `Phone` VARCHAR(20) NOT NULL
+        CHECK (LENGTH(`Phone`) = 10 AND `Phone` REGEXP "^[0-9]+$"),
+    `Created_At` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `Updated_At` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`ID`),
     UNIQUE (`Location_Name`)
 );
@@ -18,6 +22,8 @@ CREATE TABLE `Section` (
     `Location_ID` INT NOT NULL,
     `Section_Name` VARCHAR(20) NOT NULL,
     `Description` VARCHAR(200) NOT NULL,
+    `Created_At` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `Updated_At` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`ID`),
     FOREIGN KEY (`LOCATION_ID`) REFERENCES `Location`(`ID`)
         ON DELETE CASCADE
@@ -30,7 +36,10 @@ CREATE TABLE `Item` (
     `Item_Name` VARCHAR(40) NOT NULL,
     `Description` VARCHAR(200) NOT NULL,
     `Image_Url` VARCHAR(400) NOT NULL,
-    `Price` DECIMAL(6,2) NOT NULL,
+    `Price` DECIMAL(6,2) NOT NULL
+        CHECK (`Price` > 0.00),
+    `Created_At` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `Updated_At` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`ID`),
     FOREIGN KEY (`Section_ID`) REFERENCES `Section`(`ID`)
         ON DELETE CASCADE
